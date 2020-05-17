7d7087abf6e330a84ca0d195847057c3228cc642
Coco <coco.li7777@gmail.com>
2020-05-17 21:24:52 +1000

    Updates

---
+++ precache-manifest.16fb1ba3d901d6975c0bc9722b5b6554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d97412e3582965dbe69de994415c7a",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "c1038fa6fb05bfac6cb0",
+    "url": "/admin-dashboard/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "c6ce0e85aa14fd683684",
+    "url": "/admin-dashboard/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "c1038fa6fb05bfac6cb0",
+    "url": "/admin-dashboard/static/js/2.127748c7.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/admin-dashboard/static/js/2.127748c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ce0e85aa14fd683684",
+    "url": "/admin-dashboard/static/js/main.8ee6931b.chunk.js"
+  },
+  {
+    "revision": "9b78b156d41966975a3c",
+    "url": "/admin-dashboard/static/js/runtime-main.e300b17a.js"
+  }
+]);
